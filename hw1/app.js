9dc4beaf033df068a8f5fbb1e52eeab275529beb
dmitriy.belgiets <chegidim@gmail.com>
2019-12-05 08:54:22 +0200

    second commit

--- hw1/app.js
+++ hw1/app.js
@@ -2,6 +2,7 @@ const http = require('http')
 const url = require('url')
 const querystring = require('querystring')
 const path = require('path')
+const fs = require('fs');
 
 const hostname = '127.0.0.1'
 const port = 3000
@@ -18,11 +19,24 @@ const server = http.createServer((req, res) => {
   let content = ''
 
   if (pathname.includes('/echo-query/')) {
-    console.log('/echo-query/')
+    let parts = pathname.split("/");
+    content = parts[2]
   } else if (pathname.includes('/files')) {
     headers = [
       ['Content-Type', 'text/html; charset=utf-8']
     ]
+
+    // fs.readFile("AppPages/MyPage.html", function (error, pgResp) {
+    //   if (error) {
+    //     resp.writeHead(404);
+    //     resp.write('Contents you are looking are Not Found');
+    //   } else {
+    //     resp.writeHead(200, { 'Content-Type': 'text/html' });
+    //     resp.write(pgResp);
+    //   }
+    //
+    //   resp.end();
+    // });
 
     content = '<audio controls><source src="' + path.basename('data/mysong.mp3') + '" type="audio/mp3"></audio>'
   } else {
